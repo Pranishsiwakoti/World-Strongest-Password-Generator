b896ae27c3a856bc59014d5148a31402bf8df463
Pranish siwakoti <146727139+Pranishsiwakoti@users.noreply.github.com>
2024-01-31 17:30:21 +0545

    Updated - Stongest Password Generator
    Hey!
    In past here was un advanced Python code but Today only I have upgraded into ADVANCED So try it!!!!. If you wanna run this program on your Machine follow this Instruction.
    Step 1: Download Python in your Machine https://www.python.org/
    Step 2: Download This rar file / zip file and extract.
    Step 3: After these you can see there Python file which you extracted.
    Step 4: Simply Open it! and Generate Strongest Password.
    
    Important Note: This program can be upgrade again so stay tune.
                                        Happy Coding!!!!

--- Kernal - 1 Pass!@#$%^.py
+++ Kernal - 1 Pass!@#$%^.py
@@ -1,33 +1,32 @@
-print("Hey! Here you can get World Strongest Passwords")
-# Imported the Random Here.
+# Imported the Random module
 import random
-# I have add some Accessories for password and you can add what ever you want.
-lower = 'abcdefghijklmnopqrtuvwxyz'
+
+# Define character sets for password generation
+lower = 'abcdefghijklmnopqrstuvwxyz'
 upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 symbol = '!@#$%^&*'
 number = '0123456789'
-# Here, I have add Emojis. This is optional. I add this Emojis to make more Password Stronger.
-emoji = '😀😁😂🤣😎😛😜😝😡🤬'
 brackets = '{}[]()'
 letter = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 lowerletter = 'qwertyuiopasdfghjklzxcvbnm'
-# ulsym is ULTRA SYMBOL which is very hard to Guess the Passwords.
 ulsym = ',;.":*=+-_/?|<>~'
 
-# Simply Mix all the Accessories by giving '+' Plus icon.
-all = lower + upper + symbol + number + emoji + brackets + letter + lowerletter + ulsym
-# Put you password length
+# Combine all character sets
+all_characters = lower + upper + symbol + number + brackets + letter + lowerletter + ulsym
 
-length = 12
-# This is the most Important Code line which Generate the Password.
-password = "".join(random.sample(all,length))
-# After this I have add a Print to Print the passoword which have been generate. It works to visible to us.
-print("The Generated Password is: ",password )
-# Here I have add my own Sentence. You can make your own. This is the Sentence after Password have been Generated.
-print("Now you can Apply this Generated Password where you want to!")
+# Function to generate password based on user input for length
+def generate_password(length):
+    # Generate password using random.sample() to ensure no repeated characters
+    password = "".join(random.sample(all_characters, length))
+    return password
 
-# Important note:
+# Prompt the user to enter the length of the password
+length = int(input("Enter the length of the password: "))
 
-# If you want to work this Program in you Machine follow this instruction.
-# Step1: Copy this Code and Open VS Code or Other Code Editor and Press CTRL+F5 and you can view your Generated Password.
-# If any problem you can ask me in GitHUB. Happy Cooding!
+# Generate password and print it
+password = generate_password(length)
+print("The Generated Password is:", password)
+print("Now you can apply this generated password wherever you want!")
+
+# Add input prompt to prevent the window from closing immediately
+input("Press Enter to exit...")
